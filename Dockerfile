a066be572297dd5806d125f813dc4fe1b4218b82
Lando, Gabriel <gabriellando@hotmail.com>
2022-05-17 12:15:05 -0300

    Add files

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use NodeJS Latest Slim image
+FROM node:latest-slim
+
+# Create app directory 
+WORKDIR /app
+VOLUME /app/data
+
+# Install app dependencies
+COPY package.json ./
+RUN npm install
+
+# Copy remaning files
+COPY . .
+
+# Expose application port, if necessary
+EXPOSE 8000
+
+# Run JS file with Node
+CMD [ "node", "index.js" ]
+
+# How to run:
+# docker build . -t bdds-test
+# docker run -d -p 8005:8000 -v /home/gabriel/data/nodejs/BDDs-Test/data:/app/data --restart unless-stopped --name bdds-test bdds-test
---
+++ index.js
@@ -0,0 +1,87 @@
+const bodyParser = require('body-parser')
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+let data = {};
+try { data = JSON.parse(fs.readFileSync('./data/data.json', 'UTF-8')); }
+catch { data = {} }
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.post('/question', async function (req, res) {
+	const question = req.body?.question;
+	const correct = req.body?.correct;
+	const wrong = req.body?.wrong;
+	
+	if(!question || (!correct && !wrong)) {
+		res.status(400).send("Invalid data");
+		res.end();
+		return;
+	}
+
+    if (!data[question]){
+        data[question] = {
+            "corrects": [],
+            "wrongs": []
+        }
+    }
+
+    if (correct){
+        if (!data[question].corrects.includes(correct.toLowerCase()))
+	        data[question].corrects.push(correct);
+    }
+    if (wrong){
+        if (!data[question].wrongs.includes(wrong.toLowerCase()))
+	        data[question].wrongs.push(wrong);
+    }
+
+    const result = {
+        question,
+		answers: data[question]
+	}
+
+    fs.writeFileSync('./data/data.json', JSON.stringify(data, null, 2), 'UTF-8');
+
+	res.status(201).send(result);
+	res.end();
+});
+
+app.get('/question/:question', async function (req, res) {
+	const question = req.params?.question;
+
+	if(!question) {
+		res.status(400).send();
+		res.end();
+		return;
+	}
+
+	const corrects = data[question]?.corrects;
+	const wrongs = data[question]?.wrongs;
+
+	if(!corrects && !wrongs) {
+		res.status(404).send("Question not found");
+		res.end();
+		return;
+	}
+
+	const result = {
+        question,
+		answers: data[question]
+	}
+
+	res.status(200).send(result);
+	res.end();
+});
+
+app.get('/', async function (req, res) {
+	res.sendFile('index.html', { root: __dirname });
+});
+
+
+app.listen(port, function () {
+	console.log('Servidor rodando a todo pau na porta %d!\n', port);
+});
